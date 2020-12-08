81c05af6afe7d864d05bdca4c5d5918634043429
GeneiRyodanB <GeneiRyodanB@github.com>
2020-12-08 15:14:21 +0100

    Add Name form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameFormComponent } from './name-form/name-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameFormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
